13d3c45d022f9fc93b7f586901b06fa7580a49bb
sebastid <sebastien.durand71@gmail.com>
2023-02-20 23:45:03 +0000

    Commented out all lines

--- test-cnn.py
+++ test-cnn.py
@@ -2,25 +2,25 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-# Prepare dataset
-x_train, y_train = prepare_data(train_dataset)
-x_test, y_test = prepare_data(test_dataset)
+# # Prepare dataset
+# x_train, y_train = prepare_data(train_dataset)
+# x_test, y_test = prepare_data(test_dataset)
 
-# Define model
-model = keras.Sequential([
-    layers.Conv1D(64, 3, activation='relu', input_shape=(input_shape)),
-    layers.MaxPooling1D(2),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(output_shape)
-])
+# # Define model
+# model = keras.Sequential([
+#     layers.Conv1D(64, 3, activation='relu', input_shape=(input_shape)),
+#     layers.MaxPooling1D(2),
+#     layers.Flatten(),
+#     layers.Dense(128, activation='relu'),
+#     layers.Dense(output_shape)
+# ])
 
-# Compile model
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mse'])
+# # Compile model
+# model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mse'])
 
-# Train model
-model.fit(x_train, y_train, epochs=100, batch_size=32, validation_data=(x_test, y_test))
+# # Train model
+# model.fit(x_train, y_train, epochs=100, batch_size=32, validation_data=(x_test, y_test))
 
-# Evaluate model
-loss, mse = model.evaluate(x_test, y_test)
-predictions = model.predict(x_test)
+# # Evaluate model
+# loss, mse = model.evaluate(x_test, y_test)
+# predictions = model.predict(x_test)
