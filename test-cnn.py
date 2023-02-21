97ea4130c56348378f62193d3bb2da974e35bea4
sebastid <sebastien.durand71@gmail.com>
2023-02-21 00:01:52 +0000

    Tried second example from ChatGPT

--- test-cnn.py
+++ test-cnn.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras import layers
 
 # # Prepare dataset
 # x_train, y_train = prepare_data(train_dataset)
@@ -24,3 +24,36 @@ from tensorflow.keras import layers
 # # Evaluate model
 # loss, mse = model.evaluate(x_test, y_test)
 # predictions = model.predict(x_test)
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv1D, Dense
+
+# Define the CNN model
+model = Sequential()
+model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(10, 1)))
+model.add(Dense(1, activation='linear'))
+
+# Compile the model
+model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+
+# Generate dummy training data
+X_train = np.random.random((100, 10, 1))
+y_train = np.random.random((100, 1))
+
+# Train the model
+model.fit(X_train, y_train, epochs=100, validation_split=0.2)
+
+# Generate dummy testing data
+X_test = np.random.random((20, 10, 1))
+y_test = np.random.random((20, 1))
+
+# Evaluate the model
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test MAE:', score[1])
+
+# Make predictions on new data
+new_data = np.random.random((1, 10, 1))
+prediction = model.predict(new_data)
+print('Prediction:', prediction)
